552a42fff17e6b1bd2f34e9e25f8791a11369eb1
Ernesto Kawashima <ernesto.kawashima@gmail.com>
2023-02-24 16:47:27 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,10 +38,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-			} catch (ChessException e) {
+			}
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.hasNextLine();
-			} catch (InputMismatchException e) {
+			} 
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.hasNextLine();
 			}
